7e9e8a551caba6e4e59b2fdd5bc41c9ff06e11a2
reinaldo <reinaldo58972@gmail.com>
2025-06-08 22:01:24 -0300

    edit a user input post

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,67 +1,72 @@
 <?php
 
-defined('BASEPATH') OR exit ('Ação não permitida');
+defined('BASEPATH') OR exit('Ação não permitida');
 
-class Usuarios extends CI_Controller{
-    
+class Usuarios extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
-                
+
         //Definir se há sessão      
     }
-    
+
     public function index() {
-        
-        $data = array(         
+
+        $data = array(
             'titulo' => 'Usuários cadastrados',
-                    
             'styles' => array(
                 'vendor/datatables/dataTables.bootstrap4.min.css',
             ),
-            
             'scripts' => array(
                 'vendor/datatables/jquery.dataTables.min.js',
                 'vendor/datatables/dataTables.bootstrap4.min.js',
                 'vendor/datatables/app.js'
             ),
-            
             'usuarios' => $this->ion_auth->users()->result(),
         );
-        
+
 //        echo '<pre>';
 //        print_r($data['usuarios']);
 //        exit();
-        
+
         $this->load->view('layout/header', $data);
         $this->load->view('usuarios/index');
-        $this->load->view('layout/footer');           
+        $this->load->view('layout/footer');
     }
-    
-    
+
     public function edit($usuario_id = NULL) {
-        
-        if(!$usuario_id || !$this->ion_auth->user($usuario_id)->row()){
+
+        if (!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
             exit('Usuário não encontrado');
-        }else{
+        } else {
             $data = array(
-            'titulo' => 'Editar usuário',           
-            'usuario' => $this->ion_auth->user($usuario_id)->row(),
-            'perfil_usuario' => $this->ion_auth->get_users_groups($usuario_id)->row(),
-        );
-            
+                'titulo' => 'Editar usuário',
+                'usuario' => $this->ion_auth->user($usuario_id)->row(),
+                'perfil_usuario' => $this->ion_auth->get_users_groups($usuario_id)->row(),
+            );
+            /*
+             * 
+              [first_name] => Admin
+              [last_name] => istrator
+              [email] => admin@admin.com
+              [username] => administrator
+              [active] => 1
+              [perfil_usuario] => 1
+              [password] =>
+              [confirm_password] =>
+              [usuario_id] => 1
+             */
+
+
+
 //            echo '<pre>';
-//            print_r($data['perfil_usuario']);
+//            print_r($this->input->post());
 //            exit();
-            
+
             $this->load->view('layout/header', $data);
             $this->load->view('usuarios/edit');
             $this->load->view('layout/footer');
-        
         }
-        
-        
-        
     }
-    
-}
 
+}
--- application/views/usuarios/edit.php
+++ application/views/usuarios/edit.php
@@ -28,7 +28,7 @@
             </div>
             <div class="card-body">
 
-                <form>
+                <form method="POST">
                     <div class="mb-3 row">
 
                         <div class="col-md-4">
@@ -53,6 +53,13 @@
                     
                     <div class="form-group row">
                         <div class="col-md-4">
+                            <label>Usuário</label>
+                            <input type="text" class="form-control" name="username" placeholder="Seu usuário" value="<?php echo $usuario->username; ?>">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="col-md-4">            
+                            
+                            
                             <label>Ativo</label>
                             
                             <select class="form-control" name="active">
@@ -78,15 +85,25 @@
                         
                     </div>
                     
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>Senha</label>
+                            <input type="password" class="form-control" name="password" placeholder="Sua senha">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        
+                        <div class="col-md-6">
+                            <label>Confirme</label>
+                            <input type="password" class="form-control" name="confirm_password" placeholder="Confirme sua senha">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                       
+                        <input type="hidden" name="usuario_id" value="<?php echo $usuario->id ?>">
+                        
+                        
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    
+                    <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                 </form>
 
             </div>
