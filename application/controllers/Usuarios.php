0896ba658ade18d1e67a908fef83d822df44c61b
reinaldo <reinaldo58972@gmail.com>
2025-06-09 20:37:16 -0300

    show profile in the index view | delete a user

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -169,6 +169,28 @@ class Usuarios extends CI_Controller {
             }
         }
     }
+    
+    public function del($usuario_id = NULL){
+        
+        if(!$usuario_id || !$this->ion_auth->user($usuario_id)->row()){
+            
+            $this->session->set_flashdata('error', 'Usuário não encontrado');
+            redirect(usuarios);
+        }
+        
+        if($this->ion_auth->is_admin($usuario_id)){
+            $this->session->set_flashdata('error', 'O administrador não pode ser excluído');
+            redirect(usuarios);
+        }
+        
+        if($this->ion_auth->delete_user($usuario_id)){
+            $this->session->set_flashdata('sucesso', 'Usuário excluído com sucesso');
+            redirect(usuarios);
+        }else{
+            $this->session->set_flashdata('error', 'O usuário não foi excluído');
+            redirect(usuarios);
+        }
+    }
 
     public function email_check($email) {
 
