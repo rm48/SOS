cdc27f9d042be2d4aedd549acd817a20714ef204
reinaldo <reinaldo58972@gmail.com>
2025-06-10 08:55:35 -0300

    login - validate user input

---
+++ application/controllers/Login.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Login extends CI_Controller {
+
+    public function index() {
+
+        $identity = $this->security->xss_clean($this->input->post('email'));
+        $password = $this->security->xss_clean($this->input->post('password'));
+        $remember = FALSE;
+        if ($this->ion_auth->login($identity, $password, $remember)) {
+            
+            redirect('home');
+        } else {
+            $this->session->set_flashdata('error', 'Verifique seu e-mail ou senha');
+            $this->load->view('layout/header');
+            $this->load->view('login/index');
+            $this->load->view('layout/footer');
+        }
+
+    }
+
+}
--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,63 +1,68 @@
- <!-- Footer -->
-      <footer class="sticky-footer bg-white">
+<?php if (!$this->router->fetch_class() == 'login'): ?>
+
+    <!-- Footer -->
+    <footer class="sticky-footer bg-white">
         <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Service Order System <?php echo date('Y') ?>&nbsp; | By Reinaldo Martins</span>
-          </div>
+            <div class="copyright text-center my-auto">
+                <span>Copyright &copy; Service Order System <?php echo date('Y') ?>&nbsp; | By Reinaldo Martins</span>
+            </div>
         </div>
-      </footer>
-      <!-- End of Footer -->
+    </footer>
+    <!-- End of Footer -->
 
-    </div>
-    <!-- End of Content Wrapper -->
+<?php endif; ?> 
 
-  </div>
-  <!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
-  </a>
+</a>
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="login.html">Logout</a>
+            </div>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo base_url('public/vendor/jquery/jquery.min.js');?>"></script>
-  <script src="<?php echo base_url('public/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="<?php echo base_url('public/vendor/jquery/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('public/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url('public/vendor/jquery-easing/jquery.easing.min.js');?>"></script>
+<!-- Core plugin JavaScript-->
+<script src="<?php echo base_url('public/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="<?php echo base_url('public/js/sb-admin-2.min.js');?>"></script>
-  
-  <?php if(isset($scripts)): ?>
-  
-    <?php foreach($scripts as $script): ?>
-  
+<!-- Custom scripts for all pages-->
+<script src="<?php echo base_url('public/js/sb-admin-2.min.js'); ?>"></script>
+
+<?php if (isset($scripts)): ?>
+
+    <?php foreach ($scripts as $script): ?>
+
         <!-- Custom scripts for this module-->
-        <script src="<?php echo base_url('public/'.$script);?>"></script>
-  
+        <script src="<?php echo base_url('public/' . $script); ?>"></script>
+
     <?php endforeach; ?>
-  
-  <?php endif; ?>
+
+<?php endif; ?>
 
 </body>
 
