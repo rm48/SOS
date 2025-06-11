047b4799ec113d3e67bd82f75dbd82ef7bdee44b
reinaldo <reinaldo58972@gmail.com>
2025-06-10 22:48:49 -0300

    edit custumer - validate data

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -6,16 +6,16 @@ class Clientes extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        
+
         if (!$this->ion_auth->logged_in()) {
             $this->session->set_flashdata('info', 'Sua sessão expirou.');
             redirect('login');
-        }      
+        }
     }
-    
+
     public function index() {
-            
-             $data = array(
+
+        $data = array(
             'titulo' => 'Clientes cadastrados',
             'styles' => array(
                 'vendor/datatables/dataTables.bootstrap4.min.css',
@@ -27,10 +27,87 @@ class Clientes extends CI_Controller {
             ),
             'clientes' => $this->core_model->get_all('clientes'),
         );
-             
+
+//        echo '<pre>';
+//        print_r($data['clientes']);
+//        exit();
+
         $this->load->view('layout/header', $data);
         $this->load->view('clientes/index');
         $this->load->view('layout/footer');
-            
+    }
+
+    public function edit($cliente_id = NULL) {
+
+        if (!$cliente_id || !$this->core_model->get_by_id('clientes', array('cliente_id' => $cliente_id))) {
+            $this->session->set_flashdata('error', 'Cliente não encontrado');
+            redirect('clientes');
+        } else {
+
+            /*
+             * * [cliente_id] => 1
+              [cliente_data_cadastro] => 2025-06-10 21:08:37
+              [cliente_tipo] => 2
+              [cliente_nome] => Lucio
+              [cliente_sobrenome] => Souza
+              [cliente_data_nascimento] => 2020-03-28
+              [cliente_cpf_cnpj] => 50.941.516/0001-09
+              [cliente_rg_ie] =>
+              [cliente_email] =>
+              [cliente_telefone] =>
+              [cliente_celular] =>
+              [cliente_cep] =>
+              [cliente_endereco] =>
+              [cliente_numero_endereco] =>
+              [cliente_bairro] =>
+              [cliente_complemento] =>
+              [cliente_cidade] =>
+              [cliente_estado] =>
+              [cliente_ativo] => 0
+              [cliente_obs] =>
+             */
+            $this->form_validation->set_rules('cliente_nome', '', 'trim|required|min_length[4]|max_length[45]');
+            $this->form_validation->set_rules('cliente_sobrenome', '', 'trim|required|min_length[4]|max_length[150]');
+            $this->form_validation->set_rules('cliente_data_nascimento', '', 'required');
+            $this->form_validation->set_rules('cliente_cpf_cnpj', '', 'trim|required|exact_length[18]');
+            $this->form_validation->set_rules('cliente_rg_ie', '', 'trim|required|max_length[20]');
+            $this->form_validation->set_rules('cliente_email', '', 'trim|required|valid_email|max_length[50]');
+            $this->form_validation->set_rules('cliente_telefone', '', 'trim|max_length[14]');
+            $this->form_validation->set_rules('cliente_celular', '', 'trim|max_length[15]');
+            $this->form_validation->set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
+            $this->form_validation->set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
+            $this->form_validation->set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
+            $this->form_validation->set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
+            $this->form_validation->set_rules('cliente_complemento', '', 'trim|max_length[145]');
+            $this->form_validation->set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
+            $this->form_validation->set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
+            $this->form_validation->set_rules('cliente_obs', '', 'max_length[500]');
+
+            if ($this->form_validation->run()) {
+
+                echo '<pre>';
+                print_r($this->$data['clientes']);
+                exit();
+            } else {
+
+                $data = array(
+                    'titulo' => 'Atualizar cliente',
+                    'scripts' => array(
+                        'vendor/mask/jquery.mask.min.js',
+                        'vendor/mask/app.js'
+                    ),
+                    'cliente' => $this->core_model->get_by_id('clientes', array('cliente_id' => $cliente_id)),
+                );
+                
+                echo '<pre>';
+                print_r($data['cliente']);
+                exit();
+
+                $this->load->view('layout/header', $data);
+                $this->load->view('clientes/edit');
+                $this->load->view('layout/footer');
+            }
         }
+    }
+
 }
