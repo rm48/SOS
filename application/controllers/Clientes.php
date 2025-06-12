d1afaeb8cd4a80a0fa1167560d76f2688f218608
reinaldo <reinaldo58972@gmail.com>
2025-06-11 21:54:56 -0300

    valid cpf, cnpj

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -51,8 +51,8 @@ class Clientes extends CI_Controller {
               [cliente_nome] => Lucio
               [cliente_sobrenome] => Souza
               [cliente_data_nascimento] => 2020-03-28
-              [cliente_cpf_cnpj] => 50.941.516/0001-09
-              [cliente_rg_ie] =>
+              [cliente_cpf_cnpj] => 007.129.310-82  50.941.516/0001-09 40.499.503/0001-78
+              [cliente_rg_ie] =>50.678.741-2    734.47216-66
               [cliente_email] =>
               [cliente_telefone] =>
               [cliente_celular] =>
@@ -72,18 +72,14 @@ class Clientes extends CI_Controller {
             
             $cliente_tipo = $this->input->post('cliente_tipo');
                 
-//            if($cliente_tipo == 1){
-//                $this->form_validation->set_rules('cliente_cpf', '', 'trim|required|exact_length[18] | callback_valida_cpf');
-//            }else{
-//                $this->form_validation->set_rules('cliente_cnpj', '', 'trim|required|exact_length[18] | callback_valida_cnpj');
-//            }
-            
-           
-            
-            
-            
-//            $this->form_validation->set_rules('cliente_rg_ie', '', 'trim|required|max_length[20]|callback_check_rg_ie');          
-//            $this->form_validation->set_rules('cliente_email', '', 'trim|required|valid_email|max_length[50]|callback_check_email');
+            if($cliente_tipo == 1){
+                $this->form_validation->set_rules('cliente_cpf', '', 'trim|required|exact_length[14]|callback_valida_cpf');
+            }else{
+                $this->form_validation->set_rules('cliente_cnpj', '', 'trim|required|exact_length[18]|callback_valida_cnpj');
+            }
+                     
+            $this->form_validation->set_rules('cliente_rg_ie', '', 'trim|required|max_length[20]|callback_check_rg_ie');          
+            $this->form_validation->set_rules('cliente_email', '', 'trim|required|valid_email|max_length[50]|callback_check_email');
 //          
             if(!empty($this->input->post('cliente_telefone'))){
                 $this->form_validation->set_rules('cliente_telefone', '', 'trim|max_length[14]|callback_check_telefone');
@@ -93,16 +89,16 @@ class Clientes extends CI_Controller {
                 $this->form_validation->set_rules('cliente_celular', '', 'trim|max_length[15]|callback_check_celular');
             }
             
-//            
-//            
-//            $this->form_validation->set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
-//            $this->form_validation->set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
-//            $this->form_validation->set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
-//            $this->form_validation->set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
-//            $this->form_validation->set_rules('cliente_complemento', '', 'trim|max_length[145]');
-//            $this->form_validation->set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
-//            $this->form_validation->set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
-//            $this->form_validation->set_rules('cliente_obs', '', 'max_length[500]');
+            
+            
+            $this->form_validation->set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
+            $this->form_validation->set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
+            $this->form_validation->set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
+            $this->form_validation->set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
+            $this->form_validation->set_rules('cliente_complemento', '', 'trim|max_length[145]');
+            $this->form_validation->set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
+            $this->form_validation->set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
+            $this->form_validation->set_rules('cliente_obs', '', 'max_length[500]');
 
             if ($this->form_validation->run()) {
 
@@ -178,5 +174,127 @@ class Clientes extends CI_Controller {
             return TRUE;
         }
     }
+    
+    public function valida_cpf($cpf) {
+
+        if ($this->input->post('cliente_id')) {
+
+            $cliente_id = $this->input->post('cliente_id');
+
+            if ($this->core_model->get_by_id('clientes', array('cliente_id !=' => $cliente_id, 'cliente_cpf_cnpj' => $cpf))) {
+                $this->form_validation->set_message('valida_cpf', 'Este CPF já existe');
+                return FALSE;
+            }
+        }
+
+        $cpf = str_pad(preg_replace('/[^0-9]/', '', $cpf), 11, '0', STR_PAD_LEFT);
+        // Verifica se nenhuma das sequências abaixo foi digitada, caso seja, retorna falso
+        if (strlen($cpf) != 11 || $cpf == '00000000000' || $cpf == '11111111111' || $cpf == '22222222222' || $cpf == '33333333333' || $cpf == '44444444444' || $cpf == '55555555555' || $cpf == '66666666666' || $cpf == '77777777777' || $cpf == '88888888888' || $cpf == '99999999999') {
+
+            $this->form_validation->set_message('valida_cpf', 'Por favor digite um CPF válido');
+            return FALSE;
+        } else {
+            // Calcula os números para verificar se o CPF é verdadeiro
+            for ($t = 9; $t < 11; $t++) {
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c); // Para PHP com versão < 7.4
+//                    $d += $cpf[$c] * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    $this->form_validation->set_message('valida_cpf', 'Por favor digite um CPF válido');
+                    return FALSE;
+                }
+            }
+            return TRUE;
+        }
+    }
+    
+     public function valida_cnpj($cnpj) {
+
+        // Verifica se um número foi informado
+        if (empty($cnpj)) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        if ($this->input->post('cliente_id')) {
+
+            $cliente_id = $this->input->post('cliente_id');
+
+            if ($this->core_model->get_by_id('clientes', array('cliente_id !=' => $cliente_id, 'cliente_cpf_cnpj' => $cnpj))) {
+                $this->form_validation->set_message('valida_cnpj', 'Esse CNPJ já existe');
+                return FALSE;
+            }
+        }
+
+        // Elimina possivel mascara
+        $cnpj = preg_replace("/[^0-9]/", "", $cnpj);
+        $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+
+
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cnpj) != 14) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        // Verifica se nenhuma das sequências invalidas abaixo 
+        // foi digitada. Caso afirmativo, retorna falso
+        else if ($cnpj == '00000000000000' ||
+                $cnpj == '11111111111111' ||
+                $cnpj == '22222222222222' ||
+                $cnpj == '33333333333333' ||
+                $cnpj == '44444444444444' ||
+                $cnpj == '55555555555555' ||
+                $cnpj == '66666666666666' ||
+                $cnpj == '77777777777777' ||
+                $cnpj == '88888888888888' ||
+                $cnpj == '99999999999999') {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+        } else {
+
+            $j = 5;
+            $k = 6;
+            $soma1 = "";
+            $soma2 = "";
+
+            for ($i = 0; $i < 13; $i++) {
+
+                $j = $j == 1 ? 9 : $j;
+                $k = $k == 1 ? 9 : $k;
+
+                //$soma2 += ($cnpj{$i} * $k);
+
+                $soma2 = intval($soma2) + ($cnpj{$i} * $k); //Para PHP com versão < 7.4
+//                $soma2 = intval($soma2) + ($cnpj[$i] * $k); //Para PHP com versão > 7.4
+
+                if ($i < 12) {
+                    $soma1 = intval($soma1) + ($cnpj{$i} * $j); //Para PHP com versão < 7.4
+//                    $soma1 = intval($soma1) + ($cnpj[$i] * $j); //Para PHP com versão > 7.4
+                }
+
+                $k--;
+                $j--;
+            }
+
+            $digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
+            $digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
+
+            if (!($cnpj{12} == $digito1) and ( $cnpj{13} == $digito2)) {
+                $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    
+    
 
 }
