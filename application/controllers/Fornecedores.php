e17ef7954bac23072159cabf413c41f40380e578
reinaldo <reinaldo58972@gmail.com>
2025-06-13 18:27:45 -0300

    list suppliers

---
+++ application/controllers/Fornecedores.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Fornecedores extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        if (!$this->ion_auth->logged_in()) {
+            $this->session->set_flashdata('info', 'Sua sessão expirou.');
+            redirect('login');
+        }
+    }
+    
+        public function index() {
+
+        $data = array(
+            'titulo' => 'Fornecedores cadastrados',
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css',
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
+            'fornecedores' => $this->core_model->get_all('fornecedores'),
+        );
+
+//        echo '<pre>';
+//        print_r($data['fornecedores']);
+//        exit();
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('fornecedores/index');
+        $this->load->view('layout/footer');
+    }
+}
