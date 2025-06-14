7e0ef350df87cff1a4b309e435c9a36e836ed5b5
reinaldo <reinaldo58972@gmail.com>
2025-06-14 19:01:16 -0300

    list services registered in the database

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -30,6 +30,7 @@
             <a title="Gerenciar clientes"class="collapse-item" href="<?php echo base_url('clientes'); ?>"><i class="fas fa-user-tie text-gray-900"></i>&nbsp;&nbsp;Clientes</a>
             <a title="Gerenciar fornecedores" class="collapse-item" href="<?php echo base_url('fornecedores'); ?>"><i class="fas fa-user-tag text-gray-900"></i>&nbsp;&nbsp;Fornecedores</a>
             <a title="Gerenciar vendedores" class="collapse-item" href="<?php echo base_url('vendedores'); ?>"><i class="fas fa-user-secret text-gray-900"></i>&nbsp;&nbsp;Vendedores</a>
+            <a title="Gerenciar serviços" class="collapse-item" href="<?php echo base_url('servicos'); ?>"><i class="fas fa-laptop text-gray-900"></i>&nbsp;&nbsp;Serviços</a>
           </div>
         </div>
       </li>     
