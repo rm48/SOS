2c292ffa614541134d3699ac29de0e0decf05b84
reinaldo <reinaldo58972@gmail.com>
2025-06-13 22:44:40 -0300

    update supplier

--- application/controllers/Fornecedores.php
+++ application/controllers/Fornecedores.php
@@ -40,106 +40,237 @@ class Fornecedores extends CI_Controller {
     public function edit($fornecedor_id = NULL) {
 
         if (!$fornecedor_id || !$this->core_model->get_by_id('fornecedores', array('fornecedor_id' => $fornecedor_id))) {
-
             $this->session->set_flashdata('error', 'Fornecedor não encontrado');
             redirect(fornecedores);
         } else {
-            $data = array(
-                'titulo' => 'Atualizar fornecedor',
-                'scripts' => array(
-                    'vendor/mask/jquery.mask.min.js',
-                    'vendor/mask/app.js'
-                ),
-                'fornecedor' => $this->core_model->get_by_id('fornecedores', array('fornecedor_id' => $fornecedor_id)),
-            );
-
-//            echo '<pre>';
-//            print_r($data['fornecedor']);
-//            exit();
-
-            /*
-             *     [fornecedor_id] => 1
-              [fornecedor_data_cadastro] => 2025-06-13 18:16:46
-              [fornecedor_razao] => Lucio Componentes Ltda
-              [fornecedor_nome_fantasia] => Lucio Inc
-              [fornecedor_cnpj] => 25.851.950/0001-50
-              [fornecedor_ie] =>
-              [fornecedor_telefone] =>
-              [fornecedor_celular] =>
-              [fornecedor_email] => lucioinc&contato.com.br
-              [fornecedor_contato] => Fulano de Tal
-              [fornecedor_cep] =>
-              [fornecedor_endereco] =>
-              [fornecedor_numero_endereco] =>
-              [fornecedor_bairro] =>
-              [fornecedor_complemento] =>
-              [fornecedor_cidade] =>
-              [fornecedor_estado] =>
-              [fornecedor_ativo] => 1
-              [fornecedor_obs] =>
-              [fornecedor_data_alteracao] => 2025-06-13 18:16:46
-             */
 
 
-//            $this->form_validation->set_rules('first_name', '', 'trim|required');
-//            $this->form_validation->set_rules('last_name', '', 'trim|required');
-//            $this->form_validation->set_rules('email', '', 'trim|required|valid_email|callback_email_check');
-//            $this->form_validation->set_rules('username', '', 'trim|required|callback_username_check');
-//            $this->form_validation->set_rules('password', 'Senha', 'min_length[5]|max_length[255]');
-//            $this->form_validation->set_rules('confirm_password', 'Confirme', 'matches[password]');
-//
-//            if ($this->form_validation->run()) {
-//
-//                $data = elements(
-//                        array(
-//                            'first_name',
-//                            'last_name',
-//                            'email',
-//                            'username',
-//                            'active',
-//                            'password'
-//                        ), $this->input->post()
-//                );
-//
-//                $data = $this->security->xss_clean($data);
-//
-//                /* Verifica se foi passado o password */
-//                $password = $this->input->post('password');
-//
-//                if (!$password) {
-//
-//                    unset($data['password']);
-//                }
-//                //, array('id' => $usuario_id)
-//                if ($this->ion_auth->update($usuario_id, $data)) {
-//
-//                    $perfil_usuario_db = $this->ion_auth->get_users_groups($usuario_id)->row();
-//
-//                    $perfil_usuario_post = $this->input->post('perfil_usuario');
-//
-//                    /* Se for diferente, atualiza o grupo */
-//                    if ($perfil_usuario_db->id != $perfil_usuario_post) {
-//
-//                        $this->ion_auth->remove_from_group($perfil_usuario_db->id, $usuario_id);
-//                        $this->ion_auth->add_to_group($perfil_usuario_post, $usuario_id);
-//                    }
-//
-//                    $this->session->set_flashdata('sucesso', 'Dados salvos com sucesso');
-//                } else {
-//                    $this->session->set_flashdata('error', 'Erro ao salvar os dados');
-//                }
-//                redirect('usuarios');
-//            } else {
-//
-//                $data = array(
-//                    'titulo' => 'Editar usuário',
-//                    'usuario' => $this->ion_auth->user($usuario_id)->row(),
-//                    'perfil_usuario' => $this->ion_auth->get_users_groups($usuario_id)->row(),
-//                );
+            $this->form_validation->set_rules('fornecedor_razao', '', 'trim|required|min_length[4]|max_length[200]|callback_check_razao_social');
+//           
+            $this->form_validation->set_rules('fornecedor_nome_fantasia', '', 'trim|required|min_length[4]|max_length[145]|callback_check_nome_fantasia');
+            $this->form_validation->set_rules('fornecedor_cnpj', '', 'trim|required|exact_length[18]|callback_valida_cnpj');
+            $this->form_validation->set_rules('fornecedor_ie', '', 'trim|required|max_length[20]|callback_check_ie');
+            $this->form_validation->set_rules('fornecedor_email', '', 'trim|required|valid_email|max_length[50]|callback_check_email');
+            $this->form_validation->set_rules('fornecedor_telefone', '', 'trim|required|max_length[14]|callback_check_telefone');
+            $this->form_validation->set_rules('fornecedor_celular', '', 'trim|required|max_length[15]|callback_check_celular');
 
-            $this->load->view('layout/header', $data);
-            $this->load->view('fornecedores/edit');
-            $this->load->view('layout/footer');
+            $this->form_validation->set_rules('fornecedor_cep', '', 'trim|required|exact_length[9]');
+            $this->form_validation->set_rules('fornecedor_endereco', '', 'trim|required|max_length[155]');
+            $this->form_validation->set_rules('fornecedor_numero_endereco', '', 'trim|max_length[20]');
+            $this->form_validation->set_rules('fornecedor_bairro', '', 'trim|required|max_length[45]');
+            $this->form_validation->set_rules('fornecedor_complemento', '', 'trim|max_length[145]');
+            $this->form_validation->set_rules('fornecedor_cidade', '', 'trim|required|max_length[50]');
+            $this->form_validation->set_rules('fornecedor_estado', '', 'trim|required|exact_length[2]');
+            $this->form_validation->set_rules('fornecedor_obs', '', 'max_length[500]');
+
+
+            if ($this->form_validation->run()) {
+
+                $data = elements(
+                        array(
+                            'fornecedor_razao',
+                            'fornecedor_nome_fantasia',
+                            'fornecedor_cnpj',
+                            'fornecedor_ie',
+                            'fornecedor_email',
+                            'fornecedor_telefone',
+                            'fornecedor_celular',
+                            'fornecedor_endereco',
+                            'fornecedor_numero_endereco',
+                            'fornecedor_complemento',
+                            'fornecedor_bairro',
+                            'fornecedor_cep',
+                            'fornecedor_cidade',
+                            'fornecedor_estado',
+                            'fornecedor_ativo',
+                            'fornecedor_obs',
+                        ), $this->input->post()
+                );
+
+
+                $data['fornecedor_estado'] = strtoupper($this->input->post('fornecedor_estado'));
+
+                $data = html_escape($data);
+
+                $this->core_model->update('fornecedores', $data, array('fornecedor_id' => $fornecedor_id));
+
+                redirect('fornecedores');
+            } else {    
+
+
+                //erro de validação
+
+                $data = array(
+                    'titulo' => 'Atualizar fornecedor',
+                    'scripts' => array(
+                        'vendor/mask/jquery.mask.min.js',
+                        'vendor/mask/app.js'
+                    ),
+                    'fornecedor' => $this->core_model->get_by_id('fornecedores', array('fornecedor_id' => $fornecedor_id)),
+                );
+
+
+
+                $this->load->view('layout/header', $data);
+                $this->load->view('fornecedores/edit');
+                $this->load->view('layout/footer');
+            }
+        }
+    }
+
+    public function check_razao_social($fornecedor_razao) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_razao' => $fornecedor_razao, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_razao_social', 'Esta razão social já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function check_nome_fantasia($fornecedor_nome_fantasia) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_nome_fantasia' => $fornecedor_nome_fantasia, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_nome_fantasia', 'Este nomefantasia já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function check_ie($fornecedor_ie) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_ie' => $fornecedor_ie, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_ie', 'Esta inscrição estadual já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function check_email($fornecedor_email) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_email' => $fornecedor_email, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_email', 'Esse e-mail já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function check_telefone($fornecedor_telefone) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_telefone' => $fornecedor_telefone, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_telefone', 'Esse telefone já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function check_celular($fornecedor_celular) {
+
+        $fornecedor_id = $this->input->post('fornecedor_id');
+
+        if ($this->core_model->get_by_id('fornecedores', array('fornecedor_celular' => $fornecedor_celular, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_celular  ', 'Esse celular já existe');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function valida_cnpj($cnpj) {
+
+        // Verifica se um número foi informado
+        if (empty($cnpj)) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        if ($this->input->post('fornecedor_id')) {
+
+            $fornecedor_id = $this->input->post('fornecedor_id');
+
+            if ($this->core_model->get_by_id('fornecedores', array('fornecedor_id !=' => $fornecedor_id, 'fornecedor_cnpj' => $cnpj))) {
+                $this->form_validation->set_message('valida_cnpj', 'Esse CNPJ já existe');
+                return FALSE;
+            }
+        }
+
+        // Elimina possivel mascara
+        $cnpj = preg_replace("/[^0-9]/", "", $cnpj);
+        $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+
+
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cnpj) != 14) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        // Verifica se nenhuma das sequências invalidas abaixo 
+        // foi digitada. Caso afirmativo, retorna falso
+        else if ($cnpj == '00000000000000' ||
+                $cnpj == '11111111111111' ||
+                $cnpj == '22222222222222' ||
+                $cnpj == '33333333333333' ||
+                $cnpj == '44444444444444' ||
+                $cnpj == '55555555555555' ||
+                $cnpj == '66666666666666' ||
+                $cnpj == '77777777777777' ||
+                $cnpj == '88888888888888' ||
+                $cnpj == '99999999999999') {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+        } else {
+
+            $j = 5;
+            $k = 6;
+            $soma1 = "";
+            $soma2 = "";
+
+            for ($i = 0; $i < 13; $i++) {
+
+                $j = $j == 1 ? 9 : $j;
+                $k = $k == 1 ? 9 : $k;
+
+                //$soma2 += ($cnpj{$i} * $k);
+
+                $soma2 = intval($soma2) + ($cnpj{$i} * $k); //Para PHP com versão < 7.4
+//                $soma2 = intval($soma2) + ($cnpj[$i] * $k); //Para PHP com versão > 7.4
+
+                if ($i < 12) {
+                    $soma1 = intval($soma1) + ($cnpj{$i} * $j); //Para PHP com versão < 7.4
+//                    $soma1 = intval($soma1) + ($cnpj[$i] * $j); //Para PHP com versão > 7.4
+                }
+
+                $k--;
+                $j--;
+            }
+
+            $digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
+            $digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
+
+            if (!($cnpj{12} == $digito1) and ( $cnpj{13} == $digito2)) {
+                $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+                return false;
+            } else {
+                return true;
+            }
         }
     }
 
--- application/views/fornecedores/edit.php
+++ application/views/fornecedores/edit.php
@@ -169,7 +169,7 @@
                     </fieldset>
 
                     <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
-                    <a title="Voltar" href="<?php echo base_url('fornecedores'); ?>" class="btn-success btn-sm ml-4">Voltar</a>
+                    <a title="Voltar" href="<?php echo base_url($this->router->fetch_class()); ?>" class="btn-success btn-sm ml-4">Voltar</a>
                 </form>
 
             </div>
