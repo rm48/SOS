da96210eb7ecd1e8037b1544f1e7a8dcde45d1dc
reinaldo <reinaldo58972@gmail.com>
2025-06-16 19:11:25 -0300

    add/del account payable

--- application/controllers/Pagar.php
+++ application/controllers/Pagar.php
@@ -35,6 +35,57 @@ class Pagar extends CI_Controller {
         $this->load->view('layout/footer');
     }
 
+    public function add() {
+        $this->form_validation->set_rules('conta_pagar_fornecedor_id', '', 'required');
+        $this->form_validation->set_rules('conta_pagar_data_vencimento', '', 'required');
+        $this->form_validation->set_rules('conta_pagar_valor', '', 'required');
+        $this->form_validation->set_rules('conta_pagar_obs', 'Observações', 'max_length[255]');
+
+        if ($this->form_validation->run()) {
+
+            $data = elements(
+                    array(
+                        'conta_pagar_fornecedor_id',
+                        'conta_pagar_data_vencimento',
+                        'conta_pagar_valor',
+                        'conta_pagar_status',
+                        'conta_pagar_obs',
+                    ), $this->input->post()
+            );
+
+            $conta_pagar_status = $this->input->post('conta_pagar_status');
+
+            if ($conta_pagar_status == 1) {
+                $data['conta_pagar_data_pagamento'] = date('Y-m-d h:i:s');
+            }
+
+            $data = html_escape($data);
+
+            $this->core_model->insert('contas_pagar', $data, array('conta_pagar_id' => $conta_pagar_id));
+
+            redirect('pagar');
+        } else {
+
+            $data = array(
+                'titulo' => 'Contas a pagar',
+                'styles' => array(
+                    'vendor/select2/select2.min.css',
+                ),
+                'scripts' => array(
+                    'vendor/mask/jquery.mask.min.js',
+                    'vendor/mask/app.js',
+                    'vendor/select2/select2.min.js',
+                    'vendor/select2/app.js',
+                ),              
+                'fornecedores' => $this->core_model->get_all('fornecedores'),
+            );
+
+            $this->load->view('layout/header', $data);
+            $this->load->view('pagar/add');
+            $this->load->view('layout/footer');
+        }
+    }
+
     public function edit($conta_pagar_id = NULL) {
         if (!$conta_pagar_id || !$this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id))) {
             $this->session->set_flashdata('error', 'Conta não encontrada');
@@ -56,20 +107,19 @@ class Pagar extends CI_Controller {
                             'conta_pagar_status',
                             'conta_pagar_obs',
                         ), $this->input->post()
-                );            
-                
+                );
+
                 $conta_pagar_status = $this->input->post('conta_pagar_status');
-                
-                if($conta_pagar_status == 1){
+
+                if ($conta_pagar_status == 1) {
                     $data['conta_pagar_data_pagamento'] = date('Y-m-d h:i:s');
                 }
-                
+
                 $data = html_escape($data);
-                
-                $this->core_model->update('contas_pagar', $data, array('conta_pagar_id'=> $conta_pagar_id));
-                
+
+                $this->core_model->update('contas_pagar', $data, array('conta_pagar_id' => $conta_pagar_id));
+
                 redirect('pagar');
-                
             } else {
 
                 $data = array(
@@ -91,6 +141,23 @@ class Pagar extends CI_Controller {
                 $this->load->view('pagar/edit');
                 $this->load->view('layout/footer');
             }
+        }
+    }
+    
+    
+    public function del($conta_pagar_id = NULL) {
+        if (!$conta_pagar_id || !$this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id))) {
+            $this->session->set_flashdata('error', 'Conta não encontrada');
+            redirect('pagar');
+        }
+         if ($this->core_model->get_by_id('contas_pagar', array('conta_pagar_id' => $conta_pagar_id, 'conta_pagar_status => 0'))) {
+            $this->session->set_flashdata('info', 'Conta em aberto');
+            redirect('pagar');
+        }
+        else {
+
+            $this->core_model->delete('contas_pagar', array('conta_pagar_id' => $conta_pagar_id));
+            redirect('pagar');
         }
     }
 
