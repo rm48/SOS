f56765cb279910924a38c273f68cd2f82a7e8af5
reinaldo <reinaldo58972@gmail.com>
2025-06-16 18:03:11 -0300

    adjust pay controller and timezone

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -79,9 +79,9 @@ class Produtos extends CI_Controller {
                         'vendor/mask/app.js'
                     ),
                     'produto_codigo' => $this->core_model->generate_unique_code('produtos', 'numeric', 8, 'produto_codigo'),
-                    'marcas' => $this->core_model->get_all('marcas'),
-                    'categorias' => $this->core_model->get_all('categorias'),
-                    'fornecedores' => $this->core_model->get_all('fornecedores'),
+                    'marcas' => $this->core_model->get_all('marcas', array('marca_ativa' => 1)),
+                    'categorias' => $this->core_model->get_all('categorias', array('categoria_ativa' => 1)),
+                    'fornecedores' => $this->core_model->get_all('fornecedores', array('fornecedor_ativo' => 1)),
                 );
 
                 $this->load->view('layout/header', $data);
@@ -138,9 +138,9 @@ class Produtos extends CI_Controller {
                         'vendor/mask/app.js'
                     ),
                     'produto' => $this->core_model->get_by_id('produtos', array('produto_id' => $produto_id)),
-                    'marcas' => $this->core_model->get_all('marcas'),
-                    'categorias' => $this->core_model->get_all('categorias'),
-                    'fornecedores' => $this->core_model->get_all('fornecedores'),
+                    'marcas' => $this->core_model->get_all('marcas', array('marca_ativa' => 1)),
+                    'categorias' => $this->core_model->get_all('categorias', array('categoria_ativa' => 1)),
+                    'fornecedores' => $this->core_model->get_all('fornecedores', array('fornecedor_ativo' => 1)),
                 );
 
                 $this->load->view('layout/header', $data);
